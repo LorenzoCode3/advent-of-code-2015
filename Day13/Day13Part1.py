add96378083ccc49daaaba89f0d29b0264061150
LorenzoCode3 <lorenzo.era.p@gmail.com>
2025-03-07 18:11:51 +0100

    Added day 13

---
+++ Day13/Day13Part1.py
@@ -0,0 +1,35 @@
+import os
+import itertools
+
+with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as file:
+    lines = file.read()
+
+names = set()
+happiness = dict()
+
+for line in lines.splitlines():
+    (name, _, sign, happ, neighbor) = line.replace('.', '').replace('happiness units by sitting next to ', '').split()
+    names.add(name)
+    names.add(neighbor)
+    happiness.setdefault(name, dict())[neighbor] = -int(happ) if sign == 'lose' else int(happ)
+
+max_happiness = 0
+max_permutation = []
+
+for permutation in itertools.permutations(names):
+    total_happiness = 0
+    
+    for i in range(len(permutation) - 1):
+        person = permutation[i]
+        person_neighbor = permutation[i + 1]
+        total_happiness += happiness[person][person_neighbor]
+        total_happiness += happiness[person_neighbor][person]
+    total_happiness += happiness[permutation[len(permutation) - 1]][permutation[0]]
+    total_happiness += happiness[permutation[0]][permutation[len(permutation) - 1]]
+
+    if total_happiness > max_happiness:
+        max_happiness = total_happiness
+        max_permutation = permutation
+
+print("Percorso:", " -> ".join(max_permutation))
+print("Distanza:", max_happiness)
