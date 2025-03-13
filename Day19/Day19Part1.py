d3be807729ebfc6e1717021a0a4484c889549564
LorenzoCode3 <lorenzo.era.p@gmail.com>
2025-03-13 18:02:28 +0100

    Added day 19 part 1

---
+++ Day19/Day19Part1.py
@@ -0,0 +1,27 @@
+import os
+import re
+
+with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as file:
+    lines = file.read()
+
+molecules = []
+string = ""
+
+for line in lines.splitlines():
+    if "=>" in line:
+        base, replacement = line.split(" => ")
+        molecules.append((base, replacement))
+    elif line:
+        string = line
+
+string_parts = re.split(r'(?=[A-Z])', string)[1:]
+possibilities = set()
+
+for i, part in enumerate(string_parts):
+    for base, replacement in molecules:
+        if part == base:
+            new_string = string_parts[:i] + [replacement] + string_parts[i+1:]
+            possibilities.add(''.join(new_string))
+
+print(possibilities)
+print("Possibilities:", len(possibilities))
